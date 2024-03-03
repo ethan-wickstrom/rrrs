ec5a79db3f0a4d770e2c05182eb1a4c09bac61de
ethan-wickstrom <e.t.wickstrom@wustl.edu>
2024-03-03 04:38:47 -0600

    feat: added streaming for large files.

--- src/library/csv_ops/csv_loader.rs
+++ src/library/csv_ops/csv_loader.rs
@@ -1,12 +1,21 @@
 use polars::prelude::*;
-use std::error::Error;
+use indicatif::{ProgressBar};
+use parking_lot::Mutex;
+use std::sync::Arc;
 
-pub async fn load_csv(filepath: &str) -> Result<DataFrame, Box<dyn Error>> {
-    let df = LazyCsvReader::new(filepath)
+pub async fn load_csv(filepath: &str, pb: Arc<Mutex<ProgressBar>>) -> Result<LazyFrame, PolarsError> {
+    let pb = pb.lock();
+    
+    pb.set_prefix("Loading CSV...");
+    pb.set_message("Reading CSV file");
+    
+    let reader = LazyCsvReader::new(filepath)
+        .with_cache(true)
         .has_header(true)
-        .finish()
-        .expect("Error reading CSV file")
-        .collect();
-
-    Ok(df?)
+        .finish()?;
+    
+    pb.set_prefix("CSV loaded");
+    pb.finish_with_message("CSV processing completed");
+    
+    Ok(reader)
 }
