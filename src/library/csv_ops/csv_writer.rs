eb056fb3309dce6b91bfeab9430341cdfd9861ef
ethan-wickstrom <e.t.wickstrom@wustl.edu>
2024-03-03 14:29:33 -0600

    fix: issues with lazyframe conversion and sampling

--- src/library/csv_ops/csv_writer.rs
+++ src/library/csv_ops/csv_writer.rs
@@ -4,15 +4,15 @@ use std::fs::File;
 use indicatif::ProgressBar;
 use parking_lot::Mutex;
 
-pub fn write_csv(df: &mut Result<DataFrame, PolarsError>, output_dir: &str, file_name: &str, pb: Arc<Mutex<ProgressBar>>) -> (Result<(), Box<dyn Error>>, ()) {
+pub fn write_csv(df: Result<DataFrame, PolarsError>, output_dir: &str, file_name: &str, pb: Arc<Mutex<ProgressBar>>) -> (Result<(), Box<dyn Error>>, ()) {
     pb.lock().set_message("Writing CSV");
     let file = File::create(format!("{}/{}", output_dir, file_name)).unwrap();
     let mut buffer = std::io::BufWriter::new(file);
-    let df = df.as_mut().unwrap();
+    let df = df.expect("Failed to sample data");
 
     // No need to clone df, we can directly pass the reference
     (CsvWriter::new(&mut buffer)
         .include_header(true)
-        .finish(df)
+        .finish(&mut df.clone())
         .map_err(|e| e.into()), pb.lock().finish_with_message("CSV written"))
 }
--- src/library/sampler_ops/sampler.rs
+++ src/library/sampler_ops/sampler.rs
@@ -1,18 +1,23 @@
 use polars::prelude::*;
 use parking_lot::Mutex;
-use rand::{Rng, thread_rng};
-use indicatif::{ProgressBar};
+use indicatif::{ParallelProgressIterator, ProgressBar};
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
-pub fn sample_dataframe(lazy_frame: LazyFrame, sample_size: usize, total_size: usize, pb: Arc<Mutex<ProgressBar>>) -> Result<DataFrame, PolarsError> {
+pub async fn sample_dataframe(lazy_frame: LazyFrame, sample_size: usize, pb: Arc<Mutex<ProgressBar>>) -> Result<DataFrame, PolarsError> {
     let pb = pb.lock();
     pb.set_prefix("Sampling data...");
 
-    // Generate a random value for each row and filter based on a threshold.
-    let sampled_df = lazy_frame
-        .with_column(lit(thread_rng().gen_range(0.0..1.0)).alias("rand_val"))
-        .filter(col("rand_val").lt(lit(sample_size as f64 / total_size as f64)))
-        .drop(vec!["rand_val"]) // Drop the temporary random value column
-        .collect()?; // Collects the LazyFrame into a DataFrame
+    let df = lazy_frame
+        .collect_concurrently()?
+        .fetch_blocking()
+        .par_iter()
+        .progress_with(pb.clone())
+        .map(|df| df.clone())
+        .collect::<Vec<DataFrame>>()
+        .into_iter()
+        .fold(DataFrame::default(), |acc, df| acc.vstack(&df).unwrap());
+
+    let sampled_df = df.sample_n_literal(sample_size, false, true, None)?;
 
     pb.set_prefix("Sampling completed");
     pb.finish_with_message("Data sampled");
